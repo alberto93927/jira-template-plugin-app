c7695bb4c391161447c253d8af045ab8c2b1c954
Alberto Garcia <albergarcia@csumb.edu>
2025-09-26 04:13:25 -0700

    Output change

--- src/uim/index.js
+++ src/uim/index.js
@@ -4,8 +4,8 @@ uiModificationsApi.onInit(async ({ api, context }) => {
   console.log('[UIM] init', context);
   const summary = api.getFieldById('summary');
   if (summary) {
-    const current = summary.getValue?.() || '';
-    summary.setValue(`[Template] ${current}`);
+    const cur = summary.getValue?.() || '';
+    summary.setValue(`[Template] ${cur}`);
     console.log('[UIM] summary set');
   } else {
     console.warn('[UIM] summary field not found');
